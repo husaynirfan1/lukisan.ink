b078ac27cd24ea3541a45cbf87da5ef3f20f93ca
Husayn Irfan <112241008+husaynirfan1@users.noreply.github.com>
2025-06-10 02:16:07 +0800

    Fix npm scripts and preview configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: false,
+  },
+  preview: {
+    host: true,
+    port: 4173,
+    strictPort: false,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
