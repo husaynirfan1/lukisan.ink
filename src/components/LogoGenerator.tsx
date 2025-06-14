cf564211a35eb989f7cabf411b3581724b0d7f44
Husayn Irfan <112241008+husaynirfan1@users.noreply.github.com>
2025-06-15 01:23:53 +0800

    Updated LogoGenerator.tsx

--- src/components/LogoGenerator.tsx
+++ src/components/LogoGenerator.tsx
@@ -626,7 +626,7 @@ const canGenerateAll = remainingCredits >= creditsNeeded;
                       ? 'Your monthly credits reset on the 1st of each month.' 
                       : 'Your daily free generations reset at midnight.'
                     }
-                    <br />1 credit is used per aspect ratio.
+                    <br />
                     <svg className="absolute text-gray-800 h-2 w-full left-0 top-full" x="0px" y="0px" viewBox="0 0 255 255" xmlSpace="preserve"><polygon className="fill-current" points="0,0 127.5,127.5 255,0"/></svg>
                   </div>
                 </div>
